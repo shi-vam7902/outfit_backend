cc6216f1f906048672d599ef62346a1b0f3d3f60
AMIT9280 <amth988358@gmail.com>
2023-03-11 18:46:24 +0530

    Conflict solve

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require("mongoose");
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-<<<<<<< HEAD
+
 //routes 
 const sizeRoutes = require('./Routes/shared/sizeRoutes')
 const orderRoutes = require('./Routes/shared/orderRoutes')
@@ -16,13 +16,6 @@ const occasionRoutes = require('./Routes/shared/occasionRoutes')
 const patternsRoutes = require('./Routes/shared/patternsRoutes')
 const sleevesRoutes = require('./Routes/shared/sleevesRoutes')
 const styleRoutes = require('./Routes/shared/styleRoutes')
-=======
-// const URI = process.env.URI || "mongodb://127.0.0.1:27017/outfits"
-app.listen(PORT, () => {
-  console.log("Server Started on port 3000");
-});
-
-// routes
 const roleRoutes = require('./Routes/roleRoutes')
 const userRoutes = require('./Routes/customer/userRoutes')
 const supplierRoutes = require('./Routes/supplier/supplierRoutes')
@@ -35,7 +28,15 @@ const cartRoutes = require('./Routes/shared/cartRoutes')
 const customerFeedBackRoutes = require('./Routes/customer/customerFeedBackRoutes')
 const customerReviewRoutes = require('./Routes/customer/customerReviewsRoutes')
 const designerRoutes =require('./Routes/shared/designerRoutes')
-// main routes
+
+app.use("/order",orderRoutes)
+app.use("/brand",brandRoutes)
+app.use("/color",colorsRoutes)
+app.use("/material",materialRoutes)
+app.use("/occasion",occasionRoutes)
+app.use("/pattern",patternsRoutes)
+app.use("/sleeve",sleevesRoutes)
+app.use("/style",styleRoutes)
 app.use("/roles",roleRoutes)//.1
 app.use("/users",userRoutes)//2
 app.use("/suppliers",supplierRoutes)//3
@@ -48,28 +49,6 @@ app.use("/carts",cartRoutes)//9
 app.use("/feedbacks",customerFeedBackRoutes)//10
 app.use("/reviews",customerReviewRoutes)//11
 app.use("/designer",designerRoutes)//12
-
-
-mongoose.connect("mongodb+srv://root:root@cluster0.qzotymx.mongodb.net/outfits", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    keepAlive: true,
-    socketTimeoutMS: 30000,
->>>>>>> 40a4c705af900ddb96492a5f04961d7902dfacea
-
-
-
- //url routes
-app.use("/order",orderRoutes)
-app.use("/brand",brandRoutes)
-app.use("/color",colorsRoutes)
-app.use("/material",materialRoutes)
-app.use("/occasion",occasionRoutes)
-app.use("/pattern",patternsRoutes)
-app.use("/sleeve",sleevesRoutes)
-app.use("/style",styleRoutes)
- 
-
 
 //server
 app.listen(PORT,(err)=>{
@@ -89,3 +68,5 @@ mongoose.connect('mongodb+srv://amit:Amit1324@cluster0.sxuwjh9.mongodb.net/outfi
     }
 })
 
+
+ 
